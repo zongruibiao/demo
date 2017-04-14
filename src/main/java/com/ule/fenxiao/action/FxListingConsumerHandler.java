d3194d8be65ed298613ac8f9fa9d15ad0f5fdf7b
zongruibiao <zongruibiao@163.com>
2017-04-14 13:46:40 +0800

    kafka  消息

--- src/main/java/com/ule/fenxiao/action/FxListingConsumerHandler.java
+++ src/main/java/com/ule/fenxiao/action/FxListingConsumerHandler.java
@@ -2,15 +2,18 @@ package com.ule.fenxiao.action;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 /**
  * Created by zongruibiao on 2017/4/1.
  */
+@Component
 public class FxListingConsumerHandler {
-    @KafkaListener(topics = {"test01"})
-        public void listen(ConsumerRecord<?, ?> record) {
+        @KafkaListener(topics = {"test01"})
+        public void processMessage(ConsumerRecord<?, ?> record) {
+            System.out.println(record.toString());
             Optional<?> kafkaMessage = Optional.ofNullable(record.value());
             if (kafkaMessage.isPresent()) {
                 Object message = kafkaMessage.get();
